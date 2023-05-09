516e5692870ad4b93e6c04370b941d6c0568c81b
podgaietska <podgaietska.m@gmail.com>
2023-05-09 16:54:40 -0600

    initial website layout

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+// Init Glide.js
+var hero = new Glide('#glide1', {
+    type: 'slider',
+    startAt: 0,
+    autoplay: 4500,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    animationTimingFunc: 'linear',
+}).mount()
