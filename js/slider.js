0faaf68f265dd9fef888caf0552c608b716a08ae
podgaietska <podgaietska.m@gmail.com>
2023-05-10 12:19:53 -0600

    blog page added

--- js/slider.js
+++ js/slider.js
@@ -2,8 +2,8 @@
 var hero = new Glide('#glide1', {
     type: 'slider',
     startAt: 0,
-    autoplay: 4500,
-    hoverpause: true,
+    autoplay: 5000,
+    hoverpause: false,
     perView: 1,
     animationDuration: 800,
     animationTimingFunc: 'linear',
