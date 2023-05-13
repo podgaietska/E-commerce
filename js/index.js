e410379117739d72c595818a2d73905569e83530
podgaietska <podgaietska.m@gmail.com>
2023-05-12 22:03:43 -0600

    Login page setup

--- js/index.js
+++ js/index.js
@@ -23,3 +23,16 @@ if (popup){
         }, 1000);
     });
 }
+
+const registrationButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+
+registrationButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
